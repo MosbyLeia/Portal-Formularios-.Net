930f26ba93d5b12906d66636599287e4aad31c47
MosbyLeia <73869310+MosbyLeia@users.noreply.github.com>
2024-05-28 13:27:54 -0300

    Create CommonImplementations.cs

---
+++ CommonImplementations.cs
@@ -0,0 +1,6 @@
+      public static string[] GetAllowedExtensions()
+        {
+            // Lista de extensiones permitidas
+            string[] allowedExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".pdf", ".doc", ".docx" };
+            return allowedExtensions;
+        }
